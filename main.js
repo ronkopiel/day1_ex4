44d6a084d7b1ad9598180b43759b6506750e5509
ron kopiel <ronkopiel@rons-MacBook-Pro.local>
2022-12-21 16:38:43 +0200

    added darkmode functionality

--- main.js
+++ main.js
@@ -7,3 +7,15 @@ function handleClick() {
 function delayHandleClick() {
     setTimeout(handleClick, 3000);
 }
+function toggleDarkMode() {
+        let flag = document.getElementById("DarkMode").value;
+        if (flag == 'OFF' ) {
+            document.getElementById("DarkMode").value = 'ON';
+            document.body.setAttribute('class', 'dark-mode');
+            return;
+        }
+        else {
+            document.getElementById("DarkMode").value = 'OFF';
+            document.body.setAttribute('class', 'light-mode');
+        }
+}
