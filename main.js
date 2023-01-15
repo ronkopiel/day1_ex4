1ec98c6d38ae77da5961f9dd98112c8cb73596a4
ron kopiel <ronkopiel@rons-MacBook-Pro.local>
2023-01-15 17:00:05 +0200

    added grid image gallery

--- main.js
+++ main.js
@@ -56,10 +56,33 @@ const aboutMe = [
       },
       image: "/assets/Views_of_Mount_Fuji_from_Ōwakudani_20211202.jpg",
     },
+    {
+      description: {
+        hobby: `PC Enthusiast`,
+        info: `I love owning and messing around with highend PC hardware for the hell of it. `,
+      },
+      image: "/assets/gpu.jpeg",
+    },
+    {
+      description: {
+        hobby: `Animal Handler`,
+        info: `From a very young age i was surrounded with many diffrent animals in my home and i fell in love with nature.`,
+      },
+      image: "/assets/panda1200-1.jpg",
+    },
+    {
+      description: {
+        hobby: `Japanese Culture`,
+        info: `I love japanese culture, From a young age i loved thier video games and their media and recently i started learning the language and reading classic literature. `,
+      },
+      image: "/assets/Views_of_Mount_Fuji_from_Ōwakudani_20211202.jpg",
+    },
+
   ];
   
 function createImgSection(array) {
-    let container ="";
+    let imgContainer =`<div class="image-container">`;
+    let infoContainer = `<div class="info-container">`;
     for (let i = 0; i < array.length; i++) {
         let hobbyElement = "";
         let infoElement = "";
@@ -68,8 +91,14 @@ function createImgSection(array) {
         infoElement = array[i].description.info;
         imageElement = array[i].image;
         console.log(hobbyElement,infoElement,imageElement);
-        container += `<div class="image-block"><img src=${imageElement}><h1>${hobbyElement}</h1><p>${infoElement}</p></div>`
-        document.getElementById("objects").innerHTML = container;
+        imgContainer += `<div class="image-block image-${i+1}"><img src=${imageElement}></div>`;
+        infoContainer += `<div><h1>${hobbyElement}</h1><p>${infoElement}</p></div>`;
+
     }
+    imgContainer += `</div>`
+    infoContainer += `</div>`
+    const object = document.getElementById("objects");
+    object.innerHTML += imgContainer;
+    object.innerHTML += infoContainer;
 }  
 createImgSection(aboutMe);
